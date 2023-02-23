047850ec26a82e6ad7c5481af208241a2c796f06
vannet <netvan163@gmail.com>
2023-02-23 23:10:22 +0700

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,7 @@ import {toast} from "react-toastify";
 const Home = () => {
     const [data, setData]=useState({});
     const [count, setCount]=useState(1);
+    const [count1, setCount1]=useState(1);
 
     var countbtn=function(){
       if(count==1){
@@ -20,7 +21,14 @@ const Home = () => {
         setCount(count-1);
       }
     }
-    // alert(count);
+    var countbtn1=function(){
+      if(count1==1){
+        setCount1(count1+1);
+      }else{
+        setCount1(count1-1);
+      }
+    }
+    // alert(count1);
 
     useEffect(()=>{
         fireDb.child("contacts").on("value", (snapshot)=>{
@@ -46,11 +54,38 @@ const Home = () => {
             <div className='upload1' onClick={countbtn}>
                 <img src="https://cdn-icons-png.flaticon.com/512/126/126477.png" width="60%"></img>
             </div>
-            <div className='upload2' style={{display:`${count==1?"none":""}`}}>
+            <div className='upload2' onClick={countbtn1} style={{display:`${count==1?"none":""}`}}>
                 Create post
             </div>
             <div className='upload3' style={{display:`${count==1?"none":""}`}}>
                 Your profile
+            </div>
+        </div>
+        <div className='CreatePost' style={{bottom:`${count1==1?"-603px":"0px"}`,display:`${count1==1?"none":""}`}}>
+            <div style={{border:"1px solid black",height:"5%",textAlign:"center",fontSize:"21px"}}>Upload<div style={{width:"30px",height:"30px",float:"right",border:"0px solid black"}}>X</div></div>
+
+            <div style={{width:"100%",border:"1px solid black",height:"95%",overflow:"scroll"}}>
+              <label for="file" class="file-style">
+              <img src="https://i.pinimg.com/originals/30/9a/e5/309ae59b0f6d42210ce1f0ffb6c4db83.jpg" width="100px" style={{borderRadius:"10px",marginRight:"5px"}} />
+              <img src="https://i.pinimg.com/originals/a5/65/b3/a565b32ffdcd817364464481d2d58358.jpg" width="67px" style={{borderRadius:"10px"}}  />
+              <p>Upload Photo</p>
+              <img src="https://i.pinimg.com/originals/71/c9/21/71c92110d2a9871147082458f203aa96.jpg" width="100%" style={{borderRadius:"10px"}}></img>
+              </label>
+              <input type="file" id="file" />
+              
+              <br/>
+              <input type="text" className='inputform' placeholder='Name...' maxlength="20" size="20"></input><br />
+              <br/>
+              <input type="text" className='inputform' placeholder='Age...' maxlength="4" size="4"></input><br />
+              <br/>
+              <input type="text" className='inputform' placeholder='FaceBook...'></input><br />
+
+              <textarea className='inputform2' placeholder='Message' rows="4" cols="50"></textarea><br></br>
+
+              <button className='inputform3'>Send</button>
+
+              <br></br>
+              <br></br>
             </div>
         </div>
     </div>
